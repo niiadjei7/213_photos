51750c1288b566fd04d2c58acc60f4f452da6de4
JarringlyJaded <164527842+JarringlyJaded@users.noreply.github.com>
2024-04-08 22:48:15 -0400

    Added Javadoc

--- src/photosfx/controller/MainController.java
+++ src/photosfx/controller/MainController.java
@@ -5,13 +5,27 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 
+/**
+ * The MainController class controls the main view of the application.
+ * It handles initialization tasks and quitting the application.
+ */
 public class MainController {
+
+    /**
+     * Initializes the main view of the application.
+     * This method is automatically called when the FXML file is loaded.
+     * It can be used to initialize any necessary components or perform startup tasks.
+     */
     @FXML
     private void initialize() {
         // Initialize any necessary components or perform startup tasks
-
     }
 
+    /**
+     * Handles the action of quitting the application.
+     * Displays a confirmation dialog to confirm the action.
+     * If the user confirms, the application is exited.
+     */
     @FXML
     private void handleQuitApplication() {
         // Handle quitting the application
